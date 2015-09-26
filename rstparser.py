830a58320f2257b546d8c1ad8c406b6aa8e0c61f
Yangfeng Ji <jiyfeng@gmail.com>
2015-09-26 00:11:22 -0400

    Update

--- rstparser.py
+++ rstparser.py
@@ -1,17 +1,17 @@
 ## main.py
 ## Author: Yangfeng Ji
 ## Date: 09-25-2015
-## Time-stamp: <yangfeng 09/25/2015 15:59:06>
+## Time-stamp: <yangfeng 09/26/2015 00:10:59>
 
 from code.evalparser import evalparser
 from cPickle import load
 import gzip, sys
 
-def main(path):
+def main(path, draw=True):
     with gzip.open("resources/bc3200.pickle.gz") as fin:
         print 'Load Brown clusters for creating features ...'
         bcvocab = load(fin)
-    evalparser(path=path, report=False, 
+    evalparser(path=path, report=False, draw=draw,
                bcvocab=bcvocab,
                withdp=False)
 
@@ -21,6 +21,12 @@ if __name__ == '__main__':
         path = sys.argv[1]
         print 'Read files from: {}'.format(path)
         main(path)
+    elif len(sys.argv) == 3:
+        path = sys.argv[1]
+        draw = eval(sys.argv[2])
+        print 'Read files from {}'.format(path)
+        main(path, draw)
     else:
-        print "Usage: python rstparser.py file_path"
+        print "Usage: python rstparser.py file_path [draw_rst_tree]"
         print "\tfile_path - path to the segmented file"
+
