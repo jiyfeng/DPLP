c75b31db14f7d13243003250d26df83e78343b67
RangerUFO <ufownl@gmail.com>
2021-07-19 20:28:25 +0800

    Improve the shell script of corenlp

--- corenlp.sh
+++ corenlp.sh
@@ -2,19 +2,17 @@
 #
 # Runs Stanford CoreNLP.
 # Simple uses for xml and plain text output to files are:
-#    ./corenlp.sh -file filename
-#    ./corenlp.sh -file filename -outputFormat text 
+#    ./corenlp.sh 8g /path/to/target_dir
 
-scriptdir=`dirname $0`
+scriptdir="stanford-corenlp"
 
 # echo java -mx3g -cp \"$scriptdir/*\" edu.stanford.nlp.pipeline.StanfordCoreNLP $*
 
 # $1 - path
 
-PATH=$1
+JAVA_XMX=$1
+PATH=$2
 for FNAME in $PATH/*
 do
-    /usr/bin/java -mx2g -cp "$scriptdir/*" edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,ssplit,pos,lemma,ner,parse -ssplit.eolonly -tokenize.whitespace true -file $FNAME
-    # /usr/bin/java -mx2g -cp "$scriptdir/*" edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,ssplit,pos,lemma,ner,parse -file $FNAME
-    /bin/mv $(/usr/bin/basename $FNAME.xml) $PATH/
+  /usr/bin/java -Xmx$JAVA_XMX -cp "$scriptdir/*" edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,ssplit,pos,lemma,ner,parse -ssplit.eolonly -file $FNAME -outputFormat xml -outputDirectory $PATH
 done
